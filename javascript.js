ba69fa39938594bab976950f0511988807abcb6c
nicholas <nja8161995@gmail.com>
2022-08-04 10:11:26 -0500

    Update: Game is now fully functional with a ui

--- javascript.js
+++ javascript.js
@@ -8,6 +8,7 @@ const scissorsBtn = document.createElement("button");
 const resultContainer = document.querySelector("#resultContainer");
 const gameResult = document.createElement("p");
 const scoreBoard = document.createElement("p");
+const playAgain = document.createElement("button");
 
 let playerWins = 0;
 let computerWins = 0;
@@ -29,14 +30,17 @@ gameTxt.innerText = "Choose!";
 rockBtn.innerText = "Rock";
 paperBtn.innerText = "Paper";
 scissorsBtn.innerText = "Scissors"
+playAgain.innerHTML = "Play Again?"
 
 // Start Game Function
-window.onload = function newGame() {
+window.onload(newGame());
+
+function newGame() {
     gameContainer.appendChild(startTxt);
     gameContainer.appendChild(startGame);
     // Start Game once button is clicked
     startGame.addEventListener("click", () => {
-        // Score Rest
+        // Score Reset
         playerWins = 0;
         computerWins = 0;
         // Removes Start Game Text and Button
@@ -74,17 +78,7 @@ function gameChoices() {
      return computerChoice;
  }
 
- // Function no longer needed since button inputs are now used
-// // Player's Game Choice String Conversion
-// function playersPlay(playersChoice) {
-//     //Take users input and split first lett and rest
-//     playersChoice = playersChoice[0].toUpperCase() + playersChoice.slice(1).toLowerCase();
-
-//     //Return player selection
-//     return playersChoice;
-// }
-
-// // Compares computer's and player's selection to determine winner
+// Compares computer's and player's selection to determine winner
 function playRound (playerSelection, computerSelection) { 
     let results = "";
     // Rock Conditions
@@ -131,10 +125,10 @@ function playRound (playerSelection, computerSelection) {
         results = `ERROR! Something went wrong!`
     }
     // Return game result
-    console.log(results);
     return results;
 }
 
+// Logs all the scores between rounds
 function score(round) {
     //         let roundResult = playRound(gameButton, computerPlay());
     // Determine winner from round results
@@ -160,81 +154,57 @@ function score(round) {
     }
 }
 
+// Game Function to play until someone get 5 wins
 function game() {
     gameChoices();
-
     const gameOptions = document.querySelectorAll(".gameBtn");
     gameOptions.forEach(button => button.addEventListener("click", function (e) {
         let roundResult = playRound(this.innerText, computerPlay());
         let scoreArray = score(roundResult);
-        gameResult.innerText = roundResult;
-        scoreBoard.innerText = `${scoreArray[0]} - ${scoreArray[1]}`;
-        resultContainer.appendChild(gameResult);
-        resultContainer.appendChild(scoreBoard);
+        if (scoreArray[0] === 5 || scoreArray[1] === 5) {
+            if (scoreArray[0] === 5) {
+                gameOver("Player", scoreArray);
+            }
+            else {
+                gameOver("Computer", scoreArray);
+            }
+        }
+        else {
+            gameResult.innerText = roundResult;
+            scoreBoard.innerText = `Player: ${scoreArray[0]} - Computer: ${scoreArray[1]}`;
+            resultContainer.appendChild(gameResult);
+            resultContainer.appendChild(scoreBoard);
+        }
     }))
-
-    
 }
-// function game() {
-//     // Variable to count round wins
-//     let playerWins = 0;
-//     let computerWins = 0;
-//     let gameWin = false;
-//     let gameLose = false;
-//     const playButton = document.querySelectorAll("button");
-//     const gameResult = document.createElement("p");
-//     let gameButton = "";
 
-    
-    
-//     // Create a loop for 5 games
-//     for (let i = 0; i < 5; i++) {
-//         // playButton.forEach(button => button.addEventListener("click", function (e) {
-//         //     gameButton = this.value;
-//         //     gameResult.innerText = playRound(gameButton, computerPlay());
-//         //     gameResult.classList.add("gameResult");
-//         //     //resultContainer.appendChild(gameResult);
+// Displays a winner showing final scores
+function gameOver(winner, scoreArray) {
+    gameContainer.removeChild(gameTxt);
+    gameContainer.removeChild(rockBtn);
+    gameContainer.removeChild(paperBtn);
+    gameContainer.removeChild(scissorsBtn);
+    if (winner === "Player") {
+        gameResult.innerHTML = `YOU WIN!`;
+    }
+    else {
+        gameResult.innerHTML = `YOU LOSE!`;
+    }
+    scoreBoard.innerText = `Player: ${scoreArray[0]} - Computer: ${scoreArray[1]}`;
+    startOver();
+}
 
-//         // }));
-//         let roundResult = playRound(gameButton, computerPlay());
+// Displays a button to play agin
+function startOver() {
+    resultContainer.appendChild(playAgain);
+    playAgain.addEventListener("click", () => {
+        newGame();
+        resultContainer.removeChild(gameResult);       ;
+        resultContainer.removeChild(scoreBoard);
+        resultContainer.removeChild(playAgain);
+    });
+}
 
-//         // Determine winner from round results
-//         gameWin = roundResult.includes("WIN");
-//         gameLose = roundResult.includes("LOSE");
-//         // Tie and round repeats
-//         if (gameWin === false && gameLose === false) {
-//             i--;
-//             console.log(`Player: ${playerWins} - Computer: ${computerWins}`);
-//         }
-//         // Player Wins
-//         else if (gameWin === true){
-//             playerWins++; 
-//             gameLose = false
-//             console.log(`Player: ${playerWins} - Computer: ${computerWins}`);
-//         } 
-//         // Computer Wins
-//         else if (gameLose === true) {
-//             computerWins++;
-//             gameWin = false;
-//             console.log(`Player: ${playerWins} - Computer: ${computerWins}`);
-//         }
-//         // Error
-//         else {
-//             return winner = `Error: Something went wrong in the game!`
-//         }
-
-//         // First to win 3 Games win
-//         // Player wins the game
-//         if (playerWins == 3) {
-//             return winner = `YOU WIN THE GAME! Player: ${playerWins} - Computer: ${computerWins}`;
-//         }
-//         // Computer wins the game
-//         else if (computerWins == 3) {
-//             return winner = `YOU LOST THE GAME! Player: ${playerWins} - Computer: ${computerWins}`;
-//         }
-
-//     }
-// }
 
 
 
